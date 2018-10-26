c0f948fd66756c2ad014cf28d0a7be1e5ecbafd6
Huang-Libo <LiboHwang+IT@gmail.com>
2018-10-26 15:38:27 +0800

    添加工程。

---
+++ Dynamic-Icon-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dynamic-Icon-Demo
+//
+//  Created by HuangLibo on 2018/10/26.
+//  Copyright © 2018 HuangLibo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# 动态更换 APP icon
+# 动态更换 APP icon 示例
 
 系统要求：iOS 10.3 
+
+
